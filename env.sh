7399081f89a39a6c09f27b591d054d2972d5ff7a
Kyle Seongwoo Jun <njk230@gmail.com>
2020-12-24 13:33:00 +0900

    Add env.sh

---
+++ env.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+export USER_ID="username@example.com"
+export USER_PWD="password!12345"
+
+export TRAINEE_NAME="홍길동"
+export TRAINEE_BIRTH="19990101"
+
+export ENTER_DATE="20201217"
+export CLASS_NAME="예비군인/훈련병"
+export GROUP_NAME="육군"
+export UNIT_NAME="육군훈련소-논산"
--- google-news/index.ts
+++ google-news/index.ts
@@ -49,17 +49,13 @@ async function send({ id, password, name, birth, enterDate, className, groupName
   const id = process.env.USER_ID || '';
   const password = process.env.USER_PWD || '';
 
-  const name_kk = process.env.TRAINEE_NAME_KK || '';
-  const birth_kk = process.env.TRAINEE_BIRTH_KK || '';
-
-  const name_sh = process.env.TRAINEE_NAME_SH || '';
-  const birth_sh = process.env.TRAINEE_BIRTH_SH || '';
+  const name = process.env.TRAINEE_NAME || '';
+  const birth = process.env.TRAINEE_BIRTH || '';
 
   const enterDate = process.env.ENTER_DATE || '';
   const className = process.env.CLASS_NAME as thecamp.SoldierClassName;
   const groupName = process.env.GROUP_NAME as thecamp.SoldierGroupName;
   const unitName = process.env.UNIT_NAME as thecamp.SoldierUnitName;
 
-  await send({ id, password, name: name_kk, birth: birth_kk, enterDate, className, groupName, unitName });
-  await send({ id, password, name: name_sh, birth: birth_sh, enterDate, className, groupName, unitName });
+  await send({ id, password, name, birth, enterDate, className, groupName, unitName });
 })();
